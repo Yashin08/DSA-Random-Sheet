60b2b030a3cc7ad36fc0b964a64768f277af3d5b
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-03-10 22:36:52 +0530

    Create GFG_Anagram.java

---
+++ GFG_Anagram.java
@@ -0,0 +1,34 @@
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        HashMap<Character, Integer> hm1 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> hm2 = new HashMap<Character, Integer>();
+        
+        for(int i = 0; i<a.length(); i++){
+            if(hm1.containsKey(a.charAt(i))){
+                hm1.put(a.charAt(i), hm1.get(a.charAt(i))+1);
+            }
+            else{
+                hm1.put(a.charAt(i),1);
+            }
+        }
+        
+        for(int i = 0; i<b.length(); i++){
+            if(hm2.containsKey(b.charAt(i))){
+                hm2.put(b.charAt(i), hm2.get(b.charAt(i))+1);
+            }
+            else{
+                hm2.put(b.charAt(i),1);
+            }
+        }
+        
+        if(hm1.equals(hm2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
