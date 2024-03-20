be7bf3fe798b4b0c8ede8de2e225552424ff2a6b
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-03-20 23:38:22 +0530

    Create LC_121.java

---
+++ LC_121.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min_price = Integer.MAX_VALUE;
+        for(int i = 0; i<prices.length; i++){
+            min_price = Math.min(prices[i], min_price);
+            profit = Math.max(profit, (prices[i]-min_price));
+        }
+        return profit;
+    }
+}
