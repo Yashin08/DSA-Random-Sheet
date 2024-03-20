421821b7bcdb18ac8583aefe4e554c1caf3a0fd8
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-03-20 23:36:27 +0530

    Create GFG_Check_if_String_is_Rotated_by_Two_Places.java

---
+++ GFG_Check_if_String_is_Rotated_by_Two_Places.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    // Function to check if a string can be obtained by rotating
+    // another string by exactly 2 places.
+    public static boolean isRotated(String str1, String str2)
+    {
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        
+        if(str1.length() < 2) {
+            return str1.equals(str2); // If length is less than 2, both strings should be equal
+        }
+        
+        // Check rotation by 2 places to the right
+        String clockwiseRotated = str2.substring(str2.length() - 2) + str2.substring(0, str2.length() - 2);
+        if(str1.equals(clockwiseRotated)){
+            return true;
+        }
+        
+        // Check rotation by 2 places to the left
+        String anticlockwiseRotated = str2.substring(2) + str2.substring(0, 2);
+        if(str1.equals(anticlockwiseRotated)){
+            return true;
+        }
+        
+        return false;
+    }
+}
