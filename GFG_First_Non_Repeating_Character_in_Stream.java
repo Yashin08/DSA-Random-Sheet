fa5af01c9bed51822db4f357cadfdfa172307887
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-04-23 23:13:11 +0530

    Create GFG_First_Non_Repeating_Character_in_Stream.java

---
+++ GFG_First_Non_Repeating_Character_in_Stream.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String FirstNonRepeating(String A) {
+        StringBuilder result = new StringBuilder();
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < A.length(); i++) {
+            char currentChar = A.charAt(i);
+
+            // Update the count in the map
+            map.put(currentChar, map.getOrDefault(currentChar, 0) + 1);
+
+            // Append the first non-repeating character to the result
+            result.append(getFirstNonRepeating(map));
+        }
+
+        return result.toString();
+    }
+
+    private char getFirstNonRepeating(LinkedHashMap<Character, Integer> map) {
+        for (char key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return '#';
+    }
+}
