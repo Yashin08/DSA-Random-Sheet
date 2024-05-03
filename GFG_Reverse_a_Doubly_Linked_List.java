467528e147619ca397fa0f5d9b91fa011a422518
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-05-03 22:14:17 +0530

    Create GFG_Reverse_a_Doubly_Linked_List.java

---
+++ GFG_Reverse_a_Doubly_Linked_List.java
@@ -0,0 +1,14 @@
+public static Node reverseDLL(Node  head)
+{
+    Node node = head;
+    Node prevptr = null;
+    Node nextptr;
+    while(node != null){
+        nextptr = node.next;
+        node.next = prevptr; //update both the values of a single node
+        node.prev = nextptr; //update both the values of a single node
+        prevptr = node; //update the nodes
+        node = nextptr; //update the nodes
+    }
+    return prevptr;
+}
