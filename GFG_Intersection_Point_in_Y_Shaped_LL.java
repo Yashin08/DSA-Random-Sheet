13eaf205f1d623f3def7cc7ccb782b6b7f16c0fa
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-05-14 23:08:15 +0530

    Create GFG_Intersection_Point_in_Y_Shaped_LL.java

---
+++ GFG_Intersection_Point_in_Y_Shaped_LL.java
@@ -0,0 +1,22 @@
+class Intersect
+{
+    //Function to find intersection point in Y shaped Linked Lists.
+	int intersectPoint(Node head1, Node head2)
+	{
+        if(head1 == null || head2 == null){
+            return -1;
+        }
+        Node temphead1 = head1;
+        Node temphead2 = head2;
+        
+        while(temphead1 != temphead2){
+            temphead1 = temphead1 == null ? head2 : temphead1.next;
+            temphead2 = temphead2 == null ? head1 : temphead2.next;
+        }
+        
+        if(temphead1 == null){
+            return -1;
+        }
+        return temphead1.data;
+	}
+}
