da20268813d9d7668b5eb546579e8abb03d0c35c
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-05-24 22:44:55 +0530

    Update GFG_Find_First_Set_Bit.java

--- GFG_Find_First_Set_Bit.java
+++ GFG_Find_First_Set_Bit.java
@@ -1,6 +1,6 @@
 class Solution
 {
-    //Function to find position of first set bit in the given number.
+    
     public static int getFirstSetBit(int n){
             
         for(int i = 0; i<n; i++){
