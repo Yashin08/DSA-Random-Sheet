290dfdcdfbb0cae766c386db7a872c87250c771f
Rutvik Chaudhary <117923049+Yashin08@users.noreply.github.com>
2024-06-01 22:38:49 +0530

    Create LC_856.java

---
+++ LC_856.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // The initial score is 0
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(0); // Push a new score frame for the new '('
+            } else {
+                int innerScore = stack.pop(); // Pop the last score frame
+                int topScore = stack.pop(); // Pop the previous score frame
+                int newScore = topScore + Math.max(2 * innerScore, 1); // Calculate the new score
+                stack.push(newScore); // Push the new score frame back
+            }
+        }
+
+        return stack.pop();
+    }
+}
